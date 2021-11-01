5cef1de4b2bb2da8aa6fb8bbf7b34f496a50a9a1
Mustafa <mustafa010199@gmail.com>
2021-11-01 21:07:11 +0530

    first name should be of valid length user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -21,10 +21,15 @@ class UserTest < ActiveSupport::TestCase
     assert_equal ["Last name can't be blank"], @user.errors.full_messages
   end
 
-
   def test_email_should_be_present
     @user.email = ""
     assert_not @user.valid?
     assert_equal ["Email can't be blank"], @user.errors.full_messages
+  end
+
+  def test_first_name_should_be_of_valid_length
+    @user.first_name = 'A' * 51
+    assert_not @user.valid?
+    assert_equal ["First name is too long (maximum is 50 characters)"], @user.errors.full_messages
   end
 end
