def0fb0f0fc0197282a19cec20aeb5fc3914e256
Mustafa <mustafa010199@gmail.com>
2021-11-01 19:20:11 +0530

    Added length validationfor first_name and last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@
 
 class User < ApplicationRecord
   validates :email, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
 end
