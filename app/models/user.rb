dbce49fdc909c4ecb08426a857187c34f07c3495
Mustafa <mustafa010199@gmail.com>
2021-11-02 01:15:15 +0530

    "standard" made a default option for user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  enum role: { standard: "standard", administrator: "administrator" }, default: :standard
+  enum role: { standard: "standard", administrator: "administrator" }, _default: :standard
 
   validates :email, presence: true, uniqueness: true, format: { with: Constants::VALID_EMAIL_REGEX }
   validates :first_name, presence: true, length: { maximum: Constants::MAX_FIRST_NAME_LENGTH }
