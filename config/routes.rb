fc83547f831d0c581f873b8b1f36c4bb086ab84e
Mustafa <mustafa010199@gmail.com>
2021-11-02 15:40:50 +0530

    Session controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resource :sessions, only: %i[create destroy]
+
   root "home#index"
   get "*path", to: "home#index", via: :all
 end
