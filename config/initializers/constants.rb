f114c6951d60e8b2457c560ede964134951bc6cf
Mustafa <mustafa010199@gmail.com>
2021-11-02 10:40:09 +0530

    has_secure_password to added to User model

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -4,4 +4,5 @@ module Constants
   MAX_FIRST_NAME_LENGTH = 50
   MAX_SECOND_NAME_LENGTH = 50
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+  MIN_PASSWORD_LENGTH = 6
 end
