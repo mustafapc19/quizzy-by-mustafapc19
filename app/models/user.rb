d286f6a80653198d57bda8410489ae7e619fb47a
Mustafa <mustafa010199@gmail.com>
2021-11-02 23:44:46 +0530

    pr suggestion in user model pr done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+
   enum role: { standard: "standard", administrator: "administrator" }, _default: :standard
 
   has_secure_password
   has_secure_token :authentication_token
 
-  validates :email, presence: true, uniqueness: true, format: { with: Constants::VALID_EMAIL_REGEX }
-  validates :first_name, presence: true, length: { maximum: Constants::MAX_FIRST_NAME_LENGTH }
-  validates :last_name, presence: true, length: { maximum: Constants::MAX_SECOND_NAME_LENGTH }
-  validates :password, length: { minimum: Constants::MIN_PASSWORD_LENGTH }, if: -> { password.present? }
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :password, length: { minimum: 6 }, if: -> { password.present? }
   validates :password_confirmation, presence: true, on: :create
 
   before_save :to_lowercase
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 
 module Constants
-  MAX_FIRST_NAME_LENGTH = 50
-  MAX_SECOND_NAME_LENGTH = 50
-  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
-  MIN_PASSWORD_LENGTH = 6
 end
