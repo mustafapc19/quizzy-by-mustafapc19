c5c2b0e5d1d8521b2895180556bb37ea7677ee6c
Mustafa <mustafa010199@gmail.com>
2021-11-03 15:59:16 +0530

    Added listing quizes api

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 class QuizzesController < ApplicationController
-  after_action :verify_authorized
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
   before_action :authenticate_user_using_x_auth_token
+
+  def index
+    @quizzes = policy_scope(Quiz)
+  end
 
   def create
     quiz = Quiz.new(quiz_params.merge(user_id: @current_user.id))
--- app/policies/quiz_policy.rb
+++ app/policies/quiz_policy.rb
@@ -11,4 +11,17 @@ class QuizPolicy
   def create?
     quiz.user_id == user.id
   end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      scope.where(user_id: user.id)
+    end
+  end
 end
