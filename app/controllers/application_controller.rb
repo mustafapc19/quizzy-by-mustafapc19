61ae649a33d348e36627b06d40988274968b17ee
Mustafa <mustafa010199@gmail.com>
2021-11-03 12:42:38 +0530

    Added pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :handle_authorization_error
+
   def authenticate_user_using_x_auth_token
     user_email = request.headers["X-Auth-Email"]
     auth_token = request.headers["X-Auth-Token"].presence
