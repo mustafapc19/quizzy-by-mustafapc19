4cfe90c4798629b01e434ff329940bb74068eeb6
Mustafa <mustafa010199@gmail.com>
2021-11-11 17:30:15 +0530

    Improved option model

--- app/models/option.rb
+++ app/models/option.rb
@@ -4,10 +4,4 @@ class Option < ApplicationRecord
   belongs_to :question
 
   validates :name, presence: true
-
-  def self.update_or_create_by(args, attributes)
-    obj = self.find_or_create_by(args)
-    obj.update(attributes)
-    obj
-  end
 end
