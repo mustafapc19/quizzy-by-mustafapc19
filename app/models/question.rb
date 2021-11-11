3178fdcce04ec3b8e90e046d366f4e16a67a6dc3
Mustafa <mustafa010199@gmail.com>
2021-11-11 17:29:46 +0530

    Added nested delete to question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,6 @@ class Question < ApplicationRecord
   belongs_to :quiz
   has_many :options, dependent: :destroy
 
-  accepts_nested_attributes_for :options, update_only: true
+  accepts_nested_attributes_for :options, allow_destroy: true
   validates :name, presence: true
 end
